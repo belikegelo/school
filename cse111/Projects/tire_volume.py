d558e12a174f4fcf4aa7eaa6b911f907e2b8d6ea
Angelo Antimo <angeloantimo14@gmail.com>
2026-09-04 00:25:09 +0800

    update

--- cse111/Projects/tire_volume.py
+++ cse111/Projects/tire_volume.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import math
 
 width = int(input("Enter the width of the tire in mm (ex 205): "))
@@ -6,4 +7,8 @@ diameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
 
 volume = (math.pi * width ** 2 * aspect_ratio * (width * aspect_ratio + 2540 * diameter)) / 10000000000
 
+current_date_and_time = datetime.now()
+
 print (f"The approximate volume is {volume:.2f} liters")
+print(f"Date and Time: {current_date_and_time}")
+print(f"{current_date_and_time:%Y-%m-%d}")
