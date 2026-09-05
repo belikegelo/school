39e2fe7f5be0e988fa77fe16038b98c0d90a7f83
Angelo Antimo <angeloantimo14@gmail.com>
2026-09-05 11:33:43 +0800

    project complete!!!

--- cse111/Projects/tire_volume.py
+++ cse111/Projects/tire_volume.py
@@ -1,15 +1,45 @@
 from datetime import datetime
 import math
 
-width = int(input("Enter the width of the tire in mm (ex 205): "))
-aspect_ratio = int(input("Enter the aspect ratio of the tire (ex 60): "))
-diameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
+
+
+print("Welcome to the tire volume calculator!")
+
+while True:
+    
+    try:
+        width = int(input("Enter the width of the tire in mm (ex 205): "))
+        aspect_ratio = int(input("Enter the aspect ratio of the tire (ex 60): "))
+        diameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
+        break
+
+    except ValueError:
+        print("Invalid input.")
 
 volume = (math.pi * width ** 2 * aspect_ratio * (width * aspect_ratio + 2540 * diameter)) / 10000000000
 current_date_and_time = datetime.now()
+print (f"The approximate volume is {volume:.2f} liters.")
+print()
 
-print (f"The approximate volume is {volume:.2f} liters")
+while True:
+    buy = input("Do you want to buy this tire? (yes/no): ").strip().lower()
+    if buy == "yes":
+        contact = (input("Please enter your contact number: "))
+        with open("volume.txt", "at") as file:
+            print(f"{current_date_and_time:%Y-%m-%d }, {width}, {aspect_ratio}, {diameter}, {volume:.2f}, {contact}", file=file)
+            print("Thank you, we will contact you soon.")
+        break
 
-with open("volume.txt", "at") as file:
-    print(f"{current_date_and_time:%Y-%m-%d }, {width}, {aspect_ratio}, {diameter}, {volume:.2f}", file=file)
+
+    elif buy == "no":
+        #save the data to a file
+        print("Ok, Thank you! Have a nice day!")
+        with open("volume.txt", "at") as file:
+            print(f"{current_date_and_time:%Y-%m-%d }, {width}, {aspect_ratio}, {diameter}, {volume:.2f}", file=file)
+        break
+
+
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+        continue
 
