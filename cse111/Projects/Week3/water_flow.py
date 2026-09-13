ca56fd47203be7415222ec219d58d6f4c925cb7e
Angelo Antimo <angeloantimo14@gmail.com>
2026-09-14 05:52:17 +0800

    full project complete

--- cse111/Projects/Week3/water_flow.py
+++ cse111/Projects/Week3/water_flow.py
@@ -5,7 +5,8 @@ HDPE_SDR11_INNER_DIAMETER = 0.048692 # (meters)  1.917 inches
 HDPE_SDR11_FRICTION_FACTOR = 0.018   # (unitless)
 HOUSEHOLD_VELOCITY = 1.75            # (meters / second)
 WATER_DENSITY=998.2                  # density of water (998.2 kilogram / meter^3)
-
+EARTH_ACCELERATION_OF_GRAVITY = 9.8066500 #acceleration from Earths gravity 9.80665 (meter / second^2)
+DYNAMIC_VISCOSITY_OF_WATER = 0.0010016 #μ is the dynamic viscosity of water (0.0010016 Pascal seconds)
 def main():
     tower_height = float(input("Height of water tower (meters): "))
     tank_height = float(input("Height of water tank walls (meters): "))
@@ -38,11 +39,12 @@ def water_column_height(tower_height, tank_height):
 
 def pressure_gain_from_water_height(height):
     #TODO: Need to implement
-    return 0
+    numerator = WATER_DENSITY * EARTH_ACCELERATION_OF_GRAVITY * height
+    return numerator / 1000
 
 def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
     numerator = -friction_factor * pipe_length * WATER_DENSITY * fluid_velocity ** 2
-    denominator = 200 * pipe_diameter
+    denominator = 2000 * pipe_diameter
     return numerator / denominator
 
 def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
@@ -50,7 +52,8 @@ def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
 
 def reynolds_number(hydraulic_diameter, fluid_velocity):
       #TODO: Need to implement
-      return 0
+      numerator = WATER_DENSITY * hydraulic_diameter * fluid_velocity
+      return numerator / DYNAMIC_VISCOSITY_OF_WATER
 
 def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
     k=(.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter) ** 4 + 1)
