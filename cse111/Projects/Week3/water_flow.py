edb8d3d5f146583be2e9a96f0480caf40739c6ff
Angelo Antimo <angeloantimo14@gmail.com>
2026-09-14 06:23:29 +0800

    all pass 6

--- cse111/Projects/Week3/water_flow.py
+++ cse111/Projects/Week3/water_flow.py
@@ -48,7 +48,8 @@ def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_v
     return numerator / denominator
 
 def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
-    return -.04 * WATER_DENSITY * fluid_velocity * 2 * quantity_fittings / 2000
+    # Edited (I add extra * for ^2)
+    return -0.04 * WATER_DENSITY * fluid_velocity ** 2 * quantity_fittings / 2000
 
 def reynolds_number(hydraulic_diameter, fluid_velocity):
       #TODO: Need to implement
@@ -56,7 +57,8 @@ def reynolds_number(hydraulic_diameter, fluid_velocity):
       return numerator / DYNAMIC_VISCOSITY_OF_WATER
 
 def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
-    k=(.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter) ** 4 + 1)
+    # Edited from +1 to -1
+    k=(.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter) ** 4 - 1)
     return -k * WATER_DENSITY * fluid_velocity ** 2 / 2000
 
 if __name__ == "__main__":
